0abd583a643d824a9e7955cee9829f91a29fc73c
Seb Otterlab <sk01968@surrey.ac.uk>
2021-10-28 11:53:27 +0100

    essential python part in mct completed

--- MCT/Coursework 1/DEsolver.py
+++ MCT/Coursework 1/DEsolver.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import size
 import numpy.linalg as linalg
 
 
@@ -17,26 +18,42 @@ def g(x):
 
     return result
 
-x = np.linspace(-20,20,700)
-vals = g(x)
+# x = np.linspace(-20,20,700)
+# vals = g(x)
 
-plt.plot(x, vals)
-plt.plot(x, np.cos(x))
+# plt.plot(x, vals)
+# plt.plot(x, np.cos(x))
 
 
-N = 10
-dt = 1e-4
+dt = 1e-2
+N = 2*np.pi / dt
 t = np.arange(0, (N+1)*dt, dt)
 
-#building problem matrix
-plt.figure()
-plt.plot(t, g(t))
+# building problem matrix
+# plt.figure()
+# plt.plot(t, g(t))
 
-A = np.diag(g(t)) - np.diag(np.ones(x.shape), 1) - np.diag(np.ones(x.shape), -1)
+A = np.diag(2 + 4*dt**2*g(2*t)) - np.diag(np.ones(t.shape[0]-1), 1) - np.diag(np.ones(t.shape[0]-1), -1)
 A[-1,0] = -1
 A[0,-1] = -1
 
-slist = linalg.eigvals(A)
+slist, solutions = linalg.eig(A)
 print(slist)
+
+# lowest indices
+mininds = slist.argsort()
+print(mininds, slist[mininds])
+
+# set up the plot
+fig, axes = plt.subplots(5,1, sharex=True, figsize=(11,7))
+
+# fill subplots with solutions from the eigenvalue problem
+for i, ax in enumerate(axes):
+    ax.set(ylabel="w(t)", title=f"Eigenvalue s={round(slist[mininds[i]] / dt**2, 3)}")
+    ax.plot(t, solutions[:,mininds[i]], color=f"C{i}")
+    ax.grid()
+axes[-1].set(xlabel="time t")
+
+plt.tight_layout()
 
 plt.show()
