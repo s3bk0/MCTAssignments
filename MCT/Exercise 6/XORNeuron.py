059463051c5bd627ec648f34a249bac02911c17f
Seb Otterlab <sk01968@surrey.ac.uk>
2021-11-11 11:42:26 +0000

    Exercise Week 6

--- MCT/Exercise 6/XORNeuron.py
+++ MCT/Exercise 6/XORNeuron.py
@@ -5,7 +5,7 @@ def activation(x, k=1):
     return 1/(1+np.exp(-k*x))
 
 def feedForward(input, weights):
-    """propagate inputs one layer down
+    """propagate inputs one layer down a general NN
 
     Parameters
     ----------
@@ -18,24 +18,49 @@ def feedForward(input, weights):
     """
     return activation( np.sum(input * weights, axis=-1))
 
-def Neuron(points, weights):
-    return np.heaviside( np.sum( points * weights, axis=-1 ), 0 )
+def H3(I1, I2, weightsl1):
+    return activation(weightsl1[0] + weightsl1[1]*I1 + weightsl1[2]*I2)
+
+def O4(I1, I2, H3, weightsl2):
+    return activation(weightsl2[0] + weightsl2[1]*I1 + weightsl2[2]*I2 
+            + weightsl2[3]*H3)
+
+alpha = 0.1
 
 # inputs 
-x0 = np.ones(4)
 x1 = np.array([0, 0, 1, 1])
 x2 = np.array([0, 1, 0, 1])
+target = np.array([0,1,1,0])
 
-# x_i values stacked along horizontal axis -> analoguous to table
-topneurons = np.stack([x0, x1, x2], axis=1)
-hidneurons = np.zeros((4, 4))
-outneuron = np.zeros((4, 1))
-
-weightsl1 = np.random.rand(3, 3)
-weightsl1[:-1,-1] = 0
-weightsl1[[0,2], 1] = 0
-weightsl2 = np.random.rand(4, 1)
+weights = 2*np.random.rand(7) - 1
 
 # feed data forward
-hidneurons[:] = feedForward(topneurons, weightsl1)
-print(hidneurons)
+h3 = H3(x1, x2, weights[:4])
+output = O4(x1, x2, h3, weights[3:])
+
+abserror = np.sum(  0.5*(np.heaviside(output, 0) -target)**2 )
+conterror = np.sum(  0.5*(output -target)**2 )
+
+while not conterror <= 0.01:
+    for i in range(4):
+        # input vectors per layer
+        inputl1 = np.array((1, x1[i], x2[i]))
+        inputl2 = np.array((1, x1[i], x2[i], h3[i]))
+
+        # dE/dX4
+        dEdX4 = (output[i]-target[i])*(1-output[i])*output[i]
+        dEdX3 = weights[-1] * (1-h3[i])*h3[i] * dEdX4
+        # update layer 2 weights
+        weights[3:] -=  alpha* dEdX4 * inputl2
+        # update layer 1 weights
+        weights[:3] -= alpha* dEdX3 * inputl1
+
+    h3 = H3(x1, x2, weights[:4])
+    output = O4(x1, x2, h3, weights[3:])
+
+    conterror = np.sum(  0.5*(output -target)**2 )
+    abserror = np.sum(  0.5*(np.heaviside(output-0.5, 0) - target)**2 )
+
+
+print(weights)
+print(output)
